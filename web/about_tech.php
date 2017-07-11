00f4bfc6ea3dd88d614f1ad26e9ff44a11e118d4
mckema <markmckee@mm-mba.local>
2017-07-11 06:22:38 +0100

    about page on tech

---
+++ web/about_tech.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+<link href="css/owl.carousel.css" type="text/css" rel="stylesheet" />
+
+<title>PublishForce | Making research transparent</title>
+
+<meta name="keywords" content="PublishForce, PublishForce Limited, research everywhere" />
+<meta name="description" content="PublishForce is coming soon." />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="PublishForce Limited" />
+<meta name="revisit-after" content="7 days" />
+<meta name="copyright" content="PublishForce Limited" />
+
+<script type="text/javascript" src="js/mobile-nav.js"></script>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+
+<div id="header-section">
+	<div id="header-bar">
+		<!-- menu nav -->
+		<?php require("menu_teaser.php"); ?>
+        <!-- END menu nav -->
+        <div class="clr"></div>
+    </div>
+	<div class="container">
+		<div id="title-container">
+        	<h2>About PublishForce</h2>
+        </div>
+        <!-- CLOSE HEADING DIVS -->
+    </div>
+</div>
+<!-- CLOSE HEADING DIVS -->
+
+<div class="white-section">
+	<div class="container">
+        <h3>PublishForce is designed to help you</h3>
+			<p>
+				The PublishForce platform is built on an Oracle MySql database with an HTML user interface (using PHP). 
+				Thus, a user with internet access can login securely and transact with PublishForce.
+			</p>
+			<p>
+				Our system is hosted securely with Amazon Web Services.
+			</p>
+			<table style="width:680px">
+				<tr>
+					<td><img src="images/interface/robinp.jpg" /> </td>
+					<td style="width:400px">
+						Robin Poynder has a track record of 30+ years managing and developing businesses in global 
+						banking and financial services. His vision on the evolution of regulation led him to successfully 
+						develop an independent practice, highly specialised on conduct and MIFID2
+					</td>
+					<td style="width:100px">&nbsp;</td>
+				</tr>
+				<tr>
+					<td><img src="images/interface/freds.jpg" /> </td>
+					<td style="width:400px">
+						Frederic Simon has a track record of 25+ years managing operations and businesses in hedge funds 
+						and global corporate &amp; investment banking. His credo is about building business strategies 
+						around technology, innovation and regulation
+
+					</td>
+					<td style="width:100px">&nbsp;</td>
+				</tr>
+				<tr>
+					<td><img src="images/interface/markm.jpg" /> </td>
+					<td style="width:400px">
+						Mark McKee has a track record of 18 years in developing trading technology for global investment banks. 
+						Delivering technology systems that are stable, reliable, secure, and that can adapt to 
+						ever-evolving business requirements is at the core of his expertise
+
+					</td>
+					<td style="width:100px">&nbsp;</td>
+				</tr>
+			</table>
+			<!--<p>
+			The PublishForce team are bringing a new approach to the research market. 
+			Born from more than 100 years collective experience within the financial markets, the PublishForce 
+			platform draws on extensive experience in delivering and managing OTC eCommerce trading in the 
+			wholesale markets. This delivers an efficient, secure and intuitive platform where research can be bought and 
+			sold by market participants as required. Crucially, compliance is built in as an intrinsic element of the service. 
+			Ensuring that clients are only permissioned for research that is appropriate for them is part of the publication process, 
+			as is measuring and demonstrating that the compliance procedures are being met.
+			</p>-->
+    </div>   
+</div>
+
+<!-- footer section -->
+<?php require("footer_empty.php"); ?>
+<!-- END footer section -->
+
+</body>
+</html>
